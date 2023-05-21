6fcfae8c4c4838bdb3a8ee6ce8540a45e916a4b5
Khazimla <missmahomana@gmail.com>
2023-05-21 17:19:55 +0200

    complete project

---
+++ index.js
@@ -0,0 +1,27 @@
+// My global variable for getting displayed output html
+let displayedNum = document.getElementById("output-displayed")
+
+// Function for calculating input from the end user 
+function Calculate(){
+    displayedNum.value = eval(displayedNum.value);
+}
+
+// Function for displaying input from the end user
+function Display(num){
+    displayedNum.value += num;
+}
+
+// Function for deleting 1 number per time on the screen
+function Delete(){
+    displayedNum.value = displayedNum.value.slice(0,-1);
+}
+
+// Function for clearing all output displayed on the screen
+function Clear(){
+    displayedNum.value = "";
+}
+
+
+
+
+
